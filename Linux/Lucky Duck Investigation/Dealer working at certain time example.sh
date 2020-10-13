072694b209cd660273f72f77b39d929aca53f1f1
AnnabelleMacg <64769427+AnnabelleMacg@users.noreply.github.com>
2020-10-13 11:08:37 +1100

    Update Dealer working at certain time example.sh

--- Linux/Lucky Duck Investigation/Dealer working at certain time example.sh
+++ Linux/Lucky Duck Investigation/Dealer working at certain time example.sh
@@ -1,6 +1,6 @@
 #This script was designed to allow a user to look through a cassino's staff schedule and get the name of the employee working at a specific time.
 #This script was replicated to include specific times, as specified in our task. 
 
-!/bin/bash
+#!/bin/bash
 ls | cat 0315_Dealer_schedule | grep 05:00 | grep AM |
 awk -F" " '{print $1, $2, $5, $6}'
